640eeeb12e9a0fa2e38a0afcb868a3ea0be00617
哈勃-慈含 <16178283@qq.com>
2026-03-30 22:10:57 +0800

    Catalog页全反选标签checkbox时，视作不使用标签查询

--- amd/src/main.js
+++ amd/src/main.js
@@ -148,6 +148,17 @@ function loadCourses(uniqueid, $tabcontent) {
                     'values': [],
                     'type': key
                 });
+            } else if (key === 'tags') {
+                // When the block instance has configured course tags, they
+                // are shown as a checkbox list (key = "tags"). If the user
+                // unchecks all of them, send an explicit empty "tags"
+                // filter so that the backend knows tag-based filtering has
+                // been cleared and does NOT fall back to the block's
+                // default configured tags.
+                filters.push({
+                    'values': [],
+                    'type': key
+                });
             }
         });
 
